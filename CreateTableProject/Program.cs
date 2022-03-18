cbdb1101ee62436c87f4f7a6dcb8ca0beb5f783b
mladi <mladi@LAPTOP-O9A7AIAD>
2022-03-18 10:25:03 +0700

    Add project files.

---
+++ CreateTableProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CreateTableProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=LAPTOP-O9A7AIAD;database=ProdiTI;Integrated Security = TRUE");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Mahasiswa_Cobaa (NIM char(12) not null primary key, Nama varchar(50), Alamat varchar(255), Jenis_Kelamin char(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel berhasil dibuat!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops, sepertinya ada yang salah. " + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
